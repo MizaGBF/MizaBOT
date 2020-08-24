33855ed1debc146c16cb4c62bef1e1014c2af75f
MizaGBF <47531574+MizaGBF@users.noreply.github.com>
2020-08-24 12:14:47 +0200

    6.22g small change

--- cogs/gbf_access.py
+++ cogs/gbf_access.py
@@ -138,9 +138,7 @@ class GBF_Access(commands.Cog):
                             # update DB
                             if await self.bot.loop.run_in_executor(None, self.gwscrap):
                                 data = await self.GWDBver()
-                                if data[1] is None:
-                                    pass
-                                else:
+                                if data is not None and data[1] is not None:
                                     if self.bot.gw['id'] != data[1]['gw']: # different gw, we move
                                         self.bot.drive.delFiles(["GW_old.sql"], self.bot.tokens['files'])
                                         self.bot.drive.mvFile("GW.sql", self.bot.tokens['files'], "GW_old.sql")
