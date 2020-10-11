dc4c08487fa44b48192aebf50ecaa44b6528f529
MizaGBF <47531574+MizaGBF@users.noreply.github.com>
2020-10-11 13:40:27 +0200

    6.32c $gbf fix (wrong cog was called)

--- cogs/gbf_utility.py
+++ cogs/gbf_utility.py
@@ -503,7 +503,7 @@ class GBF_Utility(commands.Cog):
             await self.bot.sendError("getgwstate", str(e))
 
         try:
-            buf = self.bot.get_cog('MarchOfTheValiant').getBarrageState()
+            buf = self.bot.get_cog('DreadBarrage').getBarrageState()
             if len(buf) > 0: description += "\n" + buf
         except Exception as e:
             await self.bot.sendError("getBarrageState", str(e))
