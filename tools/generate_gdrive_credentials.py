137c3ee01e73e9cc209184476fe08a5441686a20
MizaGBF <47531574+MizaGBF@users.noreply.github.com>
2021-10-13 11:11:34 +0200

    added generate_gdrive_credentials.py

---
+++ tools/generate_gdrive_credentials.py
@@ -0,0 +1,14 @@
+from pydrive2.auth import GoogleAuth
+
+if __name__ == "__main__":
+    try:
+        gauth = GoogleAuth()
+        gauth.LoadCredentialsFile("credentials.json")
+        if self.gauth.credentials is None:
+            gauth.LocalWebserverAuth()
+            gauth.SaveCredentialsFile("credentials.json")
+            print("Credentials.json has been created")
+        else:
+            print("A valid credentials.json already exists")
+    except:
+        print("\nPlease make sure to run this file close to settings.yaml")
