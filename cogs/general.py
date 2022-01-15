531540a1e42bc719edd833365d49f2b3f4e25f54
MizaGBF <47531574+MizaGBF@users.noreply.github.com>
2022-01-15 21:35:33 +0100

    9.11g poll fix

--- cogs/general.py
+++ cogs/general.py
@@ -191,6 +191,7 @@ class General(commands.Cog):
         try:
             splitted = poll_data.split(';')
             if len(splitted) < 2: raise Exception('Specify at least a poll title and two choices\nFormat: `duration title;choice1;choice2;...;choiceN`')
+            elif len(splitted) > 20: raise Exception('A Poll is limited to 20 choices')
             embed = self.bot.util.embed(author={'name':'{} started a poll'.format(inter.author.display_name), 'icon_url':inter.author.display_avatar}, title=splitted[0], description="{} seconds remaining to vote".format(duration), color=self.color)
             view = Poll(self.bot, inter.author, embed, splitted[0], splitted[1:])
             await inter.response.send_message(embed=self.bot.util.embed(title="Information", description="Your poll is starting", color=self.color), ephemeral=True)
