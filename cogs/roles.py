ac2d00bce8cadcfe06bd9d894386c0f40af07dea
MizaGBF <47531574+MizaGBF@users.noreply.github.com>
2022-04-30 21:19:39 +0200

    9.22d defer role commands

--- cogs/roles.py
+++ cogs/roles.py
@@ -24,15 +24,16 @@ class Roles(commands.Cog):
     @role.sub_command()
     async def iam(self, inter: disnake.GuildCommandInteraction, role_name : str = commands.Param(description="The self-assignable role you want to get")):
         """Add a role to you that you choose. Role must be on a list of self-assignable roles."""
+        await inter.response.defer(ephemeral=True)
         g = str(inter.guild.id)
         roles = self.bot.data.save['assignablerole'].get(g, {})
         if role_name.lower() not in roles:
-            await inter.response.send_message(embed=self.bot.util.embed(title="Error", description="Name `{}` not found in the self-assignable role list".format(role_name), color=self.color), ephemeral=True)
+            await inter.edit_original_message(embed=self.bot.util.embed(title="Error", description="Name `{}` not found in the self-assignable role list".format(role_name), color=self.color))
         else:
             id = roles[role_name.lower()]
             r = inter.guild.get_role(id)
             if r is None:
-                await inter.response.send_message(embed=self.bot.util.embed(title="Error", description="Role `{}` not found".format(role_name), color=self.color), ephemeral=True)
+                await inter.edit_original_message(embed=self.bot.util.embed(title="Error", description="Role `{}` not found".format(role_name), color=self.color))
                 with self.bot.data.lock:
                     self.bot.data.save['assignablerole'][g].pop(role_name.lower())
                     if len(self.bot.data.save['assignablerole'][g]) == 0:
@@ -41,22 +42,23 @@ class Roles(commands.Cog):
             else:
                 try:
                     await inter.author.add_roles(r)
-                    await inter.response.send_message(embed=self.bot.util.embed(title="The command ran with success", description="Role `{}` has been added to your profile".format(r), color=self.color), ephemeral=True)
+                    await inter.edit_original_message(embed=self.bot.util.embed(title="The command ran with success", description="Role `{}` has been added to your profile".format(r), color=self.color))
                 except:
-                    await inter.response.send_message(embed=self.bot.util.embed(title="Error", description="Failed to assign the role.\nCheck if you have the role or contact a moderator.", color=self.color), ephemeral=True)
+                    await inter.edit_original_message(embed=self.bot.util.embed(title="Error", description="Failed to assign the role.\nCheck if you have the role or contact a moderator.", color=self.color))
 
     @role.sub_command()
     async def iamnot(self, inter: disnake.GuildCommandInteraction, role_name : str = commands.Param(description="The self-assignable role you want to remove")):
         """Remove a role to you that you choose. Role must be on a list of self-assignable roles."""
+        await inter.response.defer(ephemeral=True)
         g = str(inter.guild.id)
         roles = self.bot.data.save['assignablerole'].get(g, {})
         if role_name.lower() not in roles:
-            await inter.response.send_message(embed=self.bot.util.embed(title="Error", description="Name `{}` not found in the self-assignable role list".format(role_name), color=self.color), ephemeral=True)
+            await inter.edit_original_message(embed=self.bot.util.embed(title="Error", description="Name `{}` not found in the self-assignable role list".format(role_name), color=self.color))
         else:
             id = roles[role_name.lower()]
             r = inter.guild.get_role(id)
             if r is None:
-                await inter.response.send_message(embed=self.bot.util.embed(title="Error", description="Role `{}` not found".format(role_name), color=self.color), ephemeral=True)
+                await inter.edit_original_message(embed=self.bot.util.embed(title="Error", description="Role `{}` not found".format(role_name), color=self.color))
                 with self.bot.data.lock:
                     self.bot.data.save['assignablerole'][g].pop(role_name.lower())
                     if len(self.bot.data.save['assignablerole'][g]) == 0:
@@ -65,17 +67,18 @@ class Roles(commands.Cog):
             else:
                 try:
                     await inter.author.remove_roles(r)
-                    await inter.response.send_message(embed=self.bot.util.embed(title="The command ran with success", description="Role `{}` has been removed from your profile".format(r), color=self.color), ephemeral=True)
+                    await inter.edit_original_message(embed=self.bot.util.embed(title="The command ran with success", description="Role `{}` has been removed from your profile".format(r), color=self.color))
                 except:
-                    await inter.response.send_message(embed=self.bot.util.embed(title="Error", description="Failed to remove the role.\nCheck if you have the role or contact a moderator.", color=self.color), ephemeral=True)
+                    await inter.edit_original_message(embed=self.bot.util.embed(title="Error", description="Failed to remove the role.\nCheck if you have the role or contact a moderator.", color=self.color))
 
     @role.sub_command()
     async def list(self, inter: disnake.GuildCommandInteraction):
         """List the self-assignable roles available in this server"""
+        await inter.response.defer(ephemeral=True)
         g = str(inter.guild.id)
         roles = self.bot.data.save['assignablerole'].get(g, {})
         if len(roles) == 0:
-            await inter.response.send_message(embed=self.bot.util.embed(title="Error", description="No self-assignable roles available on this server", color=self.color), ephemeral=True)
+            await inter.edit_original_message(embed=self.bot.util.embed(title="Error", description="No self-assignable roles available on this server", color=self.color))
             return
         embeds = []
         fields = []
@@ -99,16 +102,17 @@ class Roles(commands.Cog):
             embeds.append(self.bot.util.embed(title="Self Assignable Roles", fields=fields, footer="Page {}/{}".format(len(embeds)+1, 1+len(roles)//30), color=self.color))
         if len(embeds) > 1:
             view = Page(self.bot, owner_id=inter.author.id, embeds=embeds)
-            await inter.response.send_message(embed=embeds[0], view=view, ephemeral=True)
+            await inter.edit_original_message(embed=embeds[0], view=view)
             view.message = await inter.original_message()
         else:
-            await inter.response.send_message(embed=embeds[0], ephemeral=True)
+            await inter.edit_original_message(embed=embeds[0])
 
     @role.sub_command()
     async def add(self, inter: disnake.GuildCommandInteraction, role_name : str = commands.Param(description="The self-assignable role you want to add to the list")):
         """Add a role to the list of self-assignable roles (Mod Only)"""
+        await inter.response.defer(ephemeral=True)
         if not self.bot.isMod(inter):
-            await inter.response.send_message(embed=self.bot.util.embed(title="Error", description="Only moderators can make a role self-assignable".format(role_name), color=self.color), ephemeral=True)
+            await inter.edit_original_message(embed=self.bot.util.embed(title="Error", description="Only moderators can make a role self-assignable".format(role_name), color=self.color))
             return
         role = None
         for r in inter.guild.roles:
@@ -116,24 +120,25 @@ class Roles(commands.Cog):
                 role = r
                 break
         if role is None:
-            await inter.response.send_message(embed=self.bot.util.embed(title="Error", description="Role `{}` not found".format(role_name), color=self.color), ephemeral=True)
+            await inter.edit_original_message(embed=self.bot.util.embed(title="Error", description="Role `{}` not found".format(role_name), color=self.color))
             return
         id = str(inter.guild.id)
         with self.bot.data.lock:
             if id not in self.bot.data.save['assignablerole']:
                 self.bot.data.save['assignablerole'][id] = {}
             if role.name.lower() in self.bot.data.save['assignablerole'][id]:
-                await inter.response.send_message(embed=self.bot.util.embed(title="Error", description="Role `{}` is already a self-assignable role.\nDid you mean `/rsar` ?".format(role_name), color=self.color), ephemeral=True)
+                await inter.edit_original_message(embed=self.bot.util.embed(title="Error", description="Role `{}` is already a self-assignable role.\nDid you mean `/rsar` ?".format(role_name), color=self.color))
                 return
             self.bot.data.save['assignablerole'][id][role.name.lower()] = role.id
             self.bot.data.pending = True
-        await inter.response.send_message(embed=self.bot.util.embed(title="Role `{}` added to the self-assignable role list".format(role_name), color=self.color), ephemeral=True)
+        await inter.edit_original_message(embed=self.bot.util.embed(title="Role `{}` added to the self-assignable role list".format(role_name), color=self.color))
 
     @role.sub_command()
     async def remove(self, inter: disnake.GuildCommandInteraction, role_name : str = commands.Param(description="The self-assignable role you want to remove from the list")):
         """Remove a role from the list of self-assignable roles (Mod Only)"""
+        await inter.response.defer(ephemeral=True)
         if not self.bot.isMod(inter):
-            await inter.response.send_message(embed=self.bot.util.embed(title="Error", description="Only moderators can make a role self-assignable".format(role_name), color=self.color), ephemeral=True)
+            await inter.edit_original_message(embed=self.bot.util.embed(title="Error", description="Only moderators can make a role self-assignable".format(role_name), color=self.color))
             return
         role = None
         for r in inter.guild.roles:
@@ -141,15 +146,15 @@ class Roles(commands.Cog):
                 role = r
                 break
         if role is None:
-            await inter.response.send_message(embed=self.bot.util.embed(title="Error", description="Role `{}` not found".format(role_name), color=self.color), ephemeral=True)
+            await inter.edit_original_message(embed=self.bot.util.embed(title="Error", description="Role `{}` not found".format(role_name), color=self.color))
             return
         id = str(inter.guild.id)
         with self.bot.data.lock:
             if id not in self.bot.data.save['assignablerole']:
                 self.bot.data.save['assignablerole'][id] = {}
             if role.name.lower() not in self.bot.data.save['assignablerole'][id]:
-                await inter.response.send_message(embed=self.bot.util.embed(title="Error", description="Role `{}` isn't a self-assignable role.\nDid you mean `/asar` ?".format(role_name), color=self.color), ephemeral=True)
+                await inter.edit_original_message(embed=self.bot.util.embed(title="Error", description="Role `{}` isn't a self-assignable role.\nDid you mean `/asar` ?".format(role_name), color=self.color))
                 return
             self.bot.data.save['assignablerole'][id].pop(role.name.lower())
             self.bot.data.pending = True
-        await inter.response.send_message(embed=self.bot.util.embed(title="Role `{}` removed from the self-assignable role list".format(role_name), color=self.color), ephemeral=True)
+        await inter.edit_original_message(embed=self.bot.util.embed(title="Role `{}` removed from the self-assignable role list".format(role_name), color=self.color))
